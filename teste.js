d00c6010d4b3fff75914ae556637c553cb601545
Thiago <heraclitothiago@hotmail.com>
2022-04-30 19:49:16 -0300

    teste

--- teste.js
+++ teste.js
@@ -1,6 +1,6 @@
 let openGoogle = window.open('https://www.google.com.br/')
 let addScript = document.createElement('script');
-addScript.setAttribute('src', 'https://raw.githubusercontent.com/heraclitothiago/assistir-legale/main/legale.js');
-addScript.setAttribute('defer', '');
+// addScript.setAttribute('src', 'https://raw.githubusercontent.com/heraclitothiago/assistir-legale/main/legale.js');
+// addScript.setAttribute('defer', '');
 addScript.innerText = `alert('teste')`;
 openGoogle.document.body.appendChild(addScript);
